acea20a0d6c541901a9230bda12145135e299659
Rakesh Tiwari <rakesh.jmi90@gmail.com>
2023-08-10 15:53:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,6 @@ st.text('🥑🍞 Avocado toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
-st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 st.dataframe(my_fruit_list)
 
