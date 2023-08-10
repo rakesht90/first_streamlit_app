9a5ce829553876af49285794e6124ab78b001b87
Rakesh Tiwari <rakesh.jmi90@gmail.com>
2023-08-10 15:26:23 +0530

    Update streamlit_app.py
    added some text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import streamlit as st
 st.title('My parents new healthy dinner')
 
 st.header('Breakfast Menu')
-
+st.text('Omega 3 & Blueberry oatmeal')
+st.text('kale, spinach & rocket smoothie ')
+st.text('Hard boiled free range eggs')
