e3f36c3e650af7dd0a40ca2facc9658d4e74e7e2
Rakesh Tiwari <rakesh.jmi90@gmail.com>
2023-08-10 15:28:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title('My parents new healthy dinner')
 
 st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry oatmeal')
-st.text('kale, spinach & rocket smoothie ')
-st.text('Hard boiled free range eggs')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
