2990907617ef6c51d809c228e85d60c490dcfbd9
Rakesh Tiwari <rakesh.jmi90@gmail.com>
2023-08-10 15:19:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title('My parents new healthy dinner')
 
-st.header('Breakfast Menu')
+st.header('_Breakfast Menu_:blue[colors] sunglasses:')
 st.header('A header with _italics_ :blue[colors] and emojis :sunglasses:')
