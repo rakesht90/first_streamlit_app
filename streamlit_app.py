73f12f5e58ea5068fe304228f026e4e308174694
Rakesh Tiwari <rakesh.jmi90@gmail.com>
2023-08-11 11:10:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,7 @@ st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 fruityvice_normalized= pandas.json_normalize(fruityvice_response.json())
 st.dataframe(fruityvice_normalized)
 
