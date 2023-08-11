b0b097e4f3fdd8dd7e4967bf6a5639055a6acd07
Rakesh Tiwari <rakesh.jmi90@gmail.com>
2023-08-11 11:01:28 +0530

    Update streamlit_app.py
    added header for fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas
-import requests
+
 
 st.title('My parents new healthy dinner')
 
@@ -17,6 +17,8 @@ fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
+st.header("Fruityvice Fruit Advice!")
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response)
 
