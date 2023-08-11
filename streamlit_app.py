9ff282dca9ab7280e2f79ed9dc67a7e9280264c1
Rakesh Tiwari <rakesh.jmi90@gmail.com>
2023-08-11 10:57:00 +0530

    Update streamlit_app.py
    a new section to display fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas
+import requests
 
 st.title('My parents new healthy dinner')
 
@@ -15,4 +16,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
