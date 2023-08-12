f68c8ac9c85059a16f23d18a1014cebf26b76737
Rakesh Tiwari <rakesh.jmi90@gmail.com>
2023-08-12 15:59:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas
-import requests
-import snowflake.connector
 
 
 st.title('My parents new healthy dinner')
@@ -20,37 +18,25 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
-
+import requests
 
 fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
 st.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 fruityvice_normalized= pandas.json_normalize(fruityvice_response.json())
 st.dataframe(fruityvice_normalized)
+import snowflake.connector
 
 st.header("Fruit load list contains:")
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from FRUIT_LOAD_LIST")
 my_data_all = my_cur.fetchall()
-fruit = pd.DataFrame(my_data_all)
+fruit= pandas.DataFrame(my_data_all)
 st.dataframe(fruit)
-
 fruit_entered = st.text_input('What fruit would you like to add?')
-if st.button('Add Fruit'):
-    my_cur.execute("insert into FRUIT_LOAD_LIST values (?)", (fruit_entered,))
-    my_cnx.commit()
-    st.write('Thanks for adding', fruit_entered)
-
+st.write('Thanks for adding', fruit_entered)
+my_cur = my_cnx.cursor()
+my_cur.execute("insert into FRUIT_LOAD_LIST values(fruit_entered)")
 my_cur.execute("select * from FRUIT_LOAD_LIST")
 my_data_all = my_cur.fetchall()
-fruit = pd.DataFrame(my_data_all)
-st.dataframe(fruit)
-
-
-
-
-
-
-
-
