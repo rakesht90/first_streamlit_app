a131ddfdcbfea31f5d671f37b17315063b782785
Rakesh Tiwari <rakesh.jmi90@gmail.com>
2023-08-12 15:15:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,10 +27,12 @@ fruityvice_normalized= pandas.json_normalize(fruityvice_response.json())
 st.dataframe(fruityvice_normalized)
 import snowflake.connector
 
+st.header("Fruit load list contains:")
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from FRUIT_LOAD_LIST;")
 my_data_row = my_cur.fetchone()
-st.text("The food load list contains:")
-st.text(my_data_row)
+//st.text("The food load list contains:")
+//st.text(my_data_row)
+st.dataframe(my_data_row)
 
